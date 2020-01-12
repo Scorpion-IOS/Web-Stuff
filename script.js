5aee33464ed6f9a18f0aacd03e61188442ef6acc
Shamil <exsample@gmail.com>
2020-01-12 00:01:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(second);
+
+let something
+console.log(something);
+let persone = {
+    name: "John",
+    age: 25,
+    isMaried: false
+};
+
+console.log(persone["name"]);
+
+ let arr = [`plum.png`,`orange.jpg`,`apple.bpm`];
+
+ console.log("arr" + "- object");
+
+//alert("Hello World!");
+
+//let answer = confirm("Are you here?");
+//console.log(answer);
+
+// // let answer = +prompt("Есть ли вам 18?","Да");
+// // console.log(typeof(answer));
+
+console.log("arr" + "- object");
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+console.log("2"==2);
+
+let isChecked = true,
+    isClosed = false;
+
+console.log(isChecked || isClosed);
+//"Coment me!"
+
+/*"And"
+    "ME!"*/
